19eb4fa5329265f95e68e950aca620589f841223
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-07 08:53:03 +0700

    Add daily predictions structure for SGP
    Added daily predictions and conclusions for SGP.
    
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-sgp.js
+++ data/data-sgp.js
@@ -1,1 +1,113 @@
+const DATA_SGP = {
 
+senin: {
+
+prediksi1: `
+ISI
+`,
+
+prediksi2: `
+ISI
+`,
+
+prediksi3: `
+ISI
+`,
+
+kesimpulan: "ISI",
+
+bbfs: "ISI",
+
+cb: "ISI"
+
+},
+
+rabu: {
+
+prediksi1: `
+ISI
+`,
+
+prediksi2: `
+ISI
+`,
+
+prediksi3: `
+ISI
+`,
+
+kesimpulan: "ISI",
+
+bbfs: "ISI",
+
+cb: "ISI"
+
+}, 
+
+kamis: {
+
+prediksi1: `
+ISI
+`,
+
+prediksi2: `
+ISI
+`,
+
+prediksi3: `
+ISI
+`,
+
+kesimpulan: "ISI",
+
+bbfs: "ISI",
+
+cb: "ISI"
+
+}, 
+
+sabtu: {
+
+prediksi1: `
+ISI
+`,
+
+prediksi2: `
+ISI
+`,
+
+prediksi3: `
+ISI
+`,
+
+kesimpulan: "ISI",
+
+bbfs: "ISI",
+
+cb: "ISI"
+
+},
+
+minggu: {
+
+prediksi1: `
+ISI
+`,
+
+prediksi2: `
+ISI
+`,
+
+prediksi3: `
+ISI
+`,
+
+kesimpulan: "ISI",
+
+bbfs: "ISI",
+
+cb: "ISI"
+
+},
+
+};

abccbac008b9d7c71113710e0767f13ade50a1db
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-08 15:01:18 +0700

    Refactor prediction keys in data-china.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-china.js
+++ data/data-china.js
@@ -1,7 +1,7 @@
 const DATA_CHINA = {
         update: "08 Juli 2026 14:15 WIB",, 
 data: {
-prediksi1: "
+prediksi: "
 5242 : 927
 0729 : 865
 2206 : 385
@@ -9,13 +9,6 @@ prediksi1: "
 9407 : 024
 5574 : 923
 ",
-prediksi2: "
-5574 : 5048
-",
-prediksi3: "
-5574 : 4726
-",
-
 kesimpulan: "43 49 20 BB",
 
         bbfs: "24390",

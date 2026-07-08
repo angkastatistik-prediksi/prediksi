c2f3f9ce4e93f538d8938c8b540193a73fadc4cb
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-08 09:24:32 +0700

    Add initial data structure for Taiwan predictions
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

---
+++ data/data-taiwan.js
@@ -0,0 +1,23 @@
+const DATA_TAIWAN = {
+
+    data: {
+
+        prediksi1: "
+          
+          ",
+        prediksi2: "
+          
+          ",
+        prediksi3: "
+          
+          ",
+
+        kesimpulan: "",
+
+        bbfs: "",
+
+        cb: ""
+
+    }
+
+};

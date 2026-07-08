4227e40ce461c23b22e375b04637f2a69438aef0
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-08 18:05:31 +0700

    Update data-taiwan.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-taiwan.js
+++ data/data-taiwan.js
@@ -1,23 +1,18 @@
 const DATA_TAIWAN = {
-
-    data: {
-
-        prediksi1: "
-          
-          ",
-        prediksi2: "
-          
-          ",
-        prediksi3: "
-          
-          ",
-
-        kesimpulan: "",
-
-        bbfs: "",
-
-        cb: ""
-
-    }
+        update: "08 Juli 2026 17:47 WIB",
+data: {
+prediksi: `
+9820 : 83
+2535 : -05
+0842 : 61
+7813 : 50
+4090 : 05
+8956 : 38
+8385 : 27
+`,
+    result2d: "52*92*70*78",
+    bbfs: "529708",
+    cb: "7"
+}
 
 };

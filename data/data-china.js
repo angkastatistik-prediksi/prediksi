a99f62866a14b1b42974ee60fb9d85f5ae070026
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-08 15:51:45 +0700

    Update DATA_CHINA with new timestamps and values
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-china.js
+++ data/data-china.js
@@ -1,5 +1,5 @@
 const DATA_CHINA = {
-        update: "08 Juli 2026 14:15 WIB",
+        update: "08 Juli 2026 15:51 WIB",
 data: {
     prediksi: `
 5242 : 927
@@ -8,10 +8,11 @@ data: {
 1032 : 927
 9407 : 024
 5574 : 923
+8682 : 301
 `,
-    result2d: "01 * 02 BB",
-    bbfs: "24390",
-    cb: "2"
+    result2d: "36*37*02*04*12*14",
+    bbfs: "3670241",
+    cb: "0"
 }
 
 };

1f5b28fc6396b421105e29ed2a87a91562ef84ac
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-09 18:13:30 +0700

    Update data-hk.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-hk.js
+++ data/data-hk.js
@@ -1,85 +1,20 @@
 const DATA_HK = {
-senin: {
-update: "06 Juli 2026 15:51 WIB",
+update: "09 Juli 2026 18:14 WIB",
 data: {
 prediksi: `
-
+4679 : 87
+0188 : 80
+6358 : 09
+2079 : 81
+3951 : 72
+3924 : 09
+4193 : 28
+8178 : 15
+8961 : 14
+5054 : 83
 `,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-selasa: {
-update: "07 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-rabu: {
-update: "08 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-kamis: {
-update: "09 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-jumat: {
-update: "10 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-sabtu: {
-update: "11 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-minggu: {
-update: "12 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
+result2d: "13*18*23*38",
+bbfs: "123856",
+cb: "3"
 }
 };

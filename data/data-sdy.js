87782354e0e61bfa271730758af99bb8973f5bae
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-09 14:59:37 +0700

    Update DATA_SDY with new date and remove days
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-sdy.js
+++ data/data-sdy.js
@@ -1,6 +1,5 @@
 const DATA_SDY = {
-senin: {
-update: "06 Juli 2026 15:51 WIB",
+update: "09 Juli 2026 14:51 WIB",
 data: {
 prediksi: `
 
@@ -8,78 +7,5 @@ prediksi: `
 result2d: "",
 bbfs: "",
 cb: ""
-}
-},
-
-selasa: {
-update: "07 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-rabu: {
-update: "08 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-kamis: {
-update: "09 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-jumat: {
-update: "10 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-sabtu: {
-update: "11 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
-},
-
-minggu: {
-update: "12 Juli 2026 15:51 WIB",
-data: {
-prediksi: `
-
-`,
-result2d: "",
-bbfs: "",
-cb: ""
-}
 }
 };

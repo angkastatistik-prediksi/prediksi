0deab6c97eb82bbb26bfa6caa2f28964606c302a
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-09 21:20:19 +0700

    Update data-sdy.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-sdy.js
+++ data/data-sdy.js
@@ -1,11 +1,22 @@
 const DATA_SDY = {
-update: "09 Juli 2026 14:51 WIB",
+update: "09 Juli 2026 21:20 WIB",
 data: {
 prediksi: `
-
+0376 : 32
+6934 : 76
+9606 : 87
+7628 : 21
+3782 : 09
+9893 : 43
+7683 : 10
+5206 : 54
+8652 : 65
+5164 : 09
+8502 : 10
 `,
-result2d: "",
-bbfs: "",
-cb: ""
+  //Rumus JUMLAH 2D TURUN 2 vs 
+result2d: "16*15*08*03",
+bbfs: "016583",
+cb: "1"
 }
 };

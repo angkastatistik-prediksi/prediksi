55cfd14486e1d603a5e70b7aaa30db1c4f929ab5
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-09 10:36:27 +0700

    Revise daily predictions and updates in data-hk.js
    Updated predictions and summaries for each day of the week.
    
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-hk.js
+++ data/data-hk.js
@@ -1,214 +1,85 @@
 const DATA_HK = {
-
 senin: {
+update: "06 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 selasa: {
+update: "07 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-4679 : 87
-0188 : 80
-6358 : 09
-2079 : 81
-3951 : 72
-3924 : 09
-4193 : 28
-8178 : 15
 `,
-
-prediksi2: `
-8172 : 57
-4679 : -90
-0188 : 28
-6358 : 67
-2079 : 41
-3951 : 20
-3924 : 90
-4193 : 82
-8178 : 03
-`,
-
-prediksi3: `
-0188 : 725
-6358 : 069
-2079 : 012
-3951 : 201
-3924 : 796
-4193 : 138
-8178 : 352
-`,
-
-kesimpulan: "15 VS 03",
-
-bbfs: "049153",
-
-cb: "0"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 rabu: {
+update: "08 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-4679 : 87
-0188 : 80
-6358 : 09
-2079 : 81
-3951 : 72
-3924 : 09
-4193 : 28
-8178 : 15
-8961 : 14
 `,
-
-prediksi2: `
-8172 : 57
-4679 :  -90
-0188 : 28
-6358 : 67
-2079 : 41
-3951 : 20
-3924 : 90
-4193 : 82 
-8178 : -03
-8961 : 19
-`,
-
-prediksi3: `
-0188 : 725
-6358 : 069
-2079 : 012
-3951 : 201
-3924 : 796
-4193 : 138
-8178 : -352
-8961 : 358
-`,
-
-kesimpulan: "14 vs 358",
-
-bbfs: "145379",
-
-cb: "1"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 kamis: {
+update: "09 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 jumat: {
+update: "10 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-8172 : 57
-4679 : -90
-0188 : 28
-6358 : 67
-2079 : 41
 `,
-
-prediksi2: `
-8172 : 57
-4679 : -90
-0188 : 28
-6358 : 67
-2079 : 41
-`,
-
-prediksi3: `
-8172 : 57
-4679 : -90
-0188 : 28
-6358 : 67
-2079 : 41
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "TES"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 sabtu: {
+update: "11 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 minggu: {
+update: "12 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
-},
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
+}
 };

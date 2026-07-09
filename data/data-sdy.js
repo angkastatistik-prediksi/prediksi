c48d70a3f642b14a7eda0858a794e6cee7d5c857
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-09 10:37:39 +0700

    Modify prediction updates and data structure
    Updated prediction data and timestamps for each day of the week.
    
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-sdy.js
+++ data/data-sdy.js
@@ -1,229 +1,85 @@
 const DATA_SDY = {
-
 senin: {
-  update: "05 Juli 2026 14:02 WIB",
+update: "06 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 selasa: {
+update: "07 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-9177 : 36
-2013 : 14
-1484 : 98
-9890 : 09
-4139 : 87
-0376 : 03
-6934 : 09
-9606 : 78
-7628 : 25
-3782 : 92
-9893 : -47
-7683 : 01
-5206 : 09
 `,
-
-prediksi2: `
-1484 : 975
-9890 : 869
-4139 : 869
-0376 : 814
-6934 : 480
-9606 : 849
-7628 : 038
-3782 : 869
-9893 : -107
-7683 : 264
-5206 : 068
-`,
-
-prediksi3: `
-9737 : 54
-8445 : 21
-4113 : -09
-9453 : -43
-6860 : 76
-9177 : 32
-2013 : -10
-1484 : -65
-9890 : -76
-4139 : 65
-0376 : -09
-6934 : 76
-9606 : 87
-7628 : 21
-3782 : 98
-9893 : 43
-7683 : 10
-5206 : 09
-`,
-
-kesimpulan: "09 VS 7628",
-
-bbfs: "097628",
-
-cb: "0"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 rabu: {
-  update: "𝚂𝙴𝙻𝙰𝚂𝙰 07 𝙹𝚄𝙻𝙸 2026 14:16 𝚆𝙸𝙱",
+update: "08 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-6934 :  76
-9606 :  87
-7628 :  21
-3782 :  09
-9893 :  43
-7683 :  10
-5206 :  54
-8652 :  65
 `,
-
-prediksi2: `
-9893 : 84
-7683 : 40
-5206 : 72
-8652 : 51
-`,
-
-prediksi3: `
-3782 : 43
-9893 : 48
-7683 : -54
-5206 : 52
-8652 : 76
-`,
-
-kesimpulan: "51 vs 76",
-
-bbfs: "517639",
-
-cb: "5"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 kamis: {
-update: "𝚁𝙰𝙱𝚄 𝟶𝟾 𝙹𝚄𝙻𝙸 𝟸𝟶𝟸𝟼 𝟷𝟺:𝟶𝟸 𝚆𝙸𝙱",
-prediksi1: `
-6934 :  76
-9606 :  87
-7628 :  21
-3782 :  09
-9893 :  43
-7683 :  10
-5206 :  54
-8652 :  65
-5164 :  09
+update: "09 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
+
 `,
-
-prediksi2: `
-9893 : 8374
-7683 : 4930
-5206 : 7263
-8652 : 5041
-5164 : 6152 
-`,
-
-prediksi3: `
-3782 : 43
-9893 : 48
-7683 : -54
-5206 : 52
-8652 : 76
-5164 : 64
-`,
-
-kesimpulan: "09 vs 64",
-
-bbfs: "614580",
-
-cb: "6"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 jumat: {
+update: "10 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 sabtu: {
+update: "11 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
 },
 
 minggu: {
+update: "12 Juli 2026 15:51 WIB",
+data: {
+prediksi: `
 
-prediksi1: `
-ISI
 `,
-
-prediksi2: `
-ISI
-`,
-
-prediksi3: `
-ISI
-`,
-
-kesimpulan: "ISI",
-
-bbfs: "ISI",
-
-cb: "ISI"
-
-},
-
+result2d: "",
+bbfs: "",
+cb: ""
+}
+}
 };

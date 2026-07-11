11be02e2931a62c5f2af102eb3fdb22b78de3e9c
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-11 13:19:42 +0700

    Update Cambodia data and predictions
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-cambodia.js
+++ data/data-cambodia.js
@@ -1,23 +1,28 @@
 const DATA_CAMBODIA = {
-        update: "Jumat 10 Juli 2026 14:14 WIB",
+        update: "Sabtu 11 Juli 2026 13:14 WIB",
 data: {
     prediksi: `
-0902 : 89
-6358 : 56
-4753 : 24
-5674 : 81
-6468 : 35
-7356 : -42
-6290 : 82
-2528 : 64
-2065 : 00
-1207 : -85
-6126 : 02
+2065 : 37
+1207 : 64
+6126 : 85
+6781 : 02
 `,
-        //Rumus sama ekor mundur wajar vs cop maju wajar
+        //Rumus sama ekor mmundu
+        //0902 : 
+//6358 : 
+//4753 : 
+//5674 : 
+//6468 : 
+//7356 : 
+//6290 : pangkas yg sudah result 4d
+//2528 : 6475 647 bawah kanan
+//2065 : 3786 378 37
+//1207 : 8647 864 64
+//6126 : 0285 085 85
+//6781 : 5602 502 02
         
-    result2d: "01*05*06*52*12*62",
-    bbfs: "510276",
+    result2d: "07*27*08*21*22",
+    bbfs: "07281",
     cb: "0"
 }
 

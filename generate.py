66ed16ffb362cb7db41e05b6c6e6561f53631611
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-14 06:48:47 +0700

    Add generate.py with market and month definitions
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

---
+++ generate.py
@@ -0,0 +1,35 @@
+import os
+import re
+from datetime import datetime
+
+MARKETS = [
+    "hk",
+    "sdy",
+    "sgp",
+    "china",
+    "japan",
+    "taiwan",
+    "cambodia"
+]
+
+MONTH = {
+    "01":"januari",
+    "02":"februari",
+    "03":"maret",
+    "04":"april",
+    "05":"mei",
+    "06":"juni",
+    "07":"juli",
+    "08":"agustus",
+    "09":"september",
+    "10":"oktober",
+    "11":"november",
+    "12":"desember"
+}
+
+def slug(date):
+    y,m,d = date.split("-")
+    return f"{int(d)}-{MONTH[m]}-{y}"
+
+def ensure(folder):
+    os.makedirs(folder, exist_ok=True)

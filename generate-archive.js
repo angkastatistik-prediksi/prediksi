e8190f0dfe77d395b110553d751f7c4ce56afb1b
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-13 20:23:15 +0700

    Update generate-archive.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- generate-archive.js
+++ generate-archive.js
@@ -1,1 +1,11 @@
+const fs = require("fs");
+const path = require("path");
 
+function generateSitemap(){}
+function generateArchive(){}
+function generateRSS(){}
+function updateHistory(){}
+generateArchive();
+generateSitemap();
+generateRSS();
+updateHistory();

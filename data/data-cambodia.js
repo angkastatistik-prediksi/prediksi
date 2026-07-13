328a21c753efe47cb9db211ddabeb0ae0e6a68c1
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-13 12:24:20 +0700

    Update data-cambodia.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-cambodia.js
+++ data/data-cambodia.js
@@ -1,12 +1,14 @@
 const DATA_CAMBODIA = {
-    update: "Minggu 12 Juli 2026 01:48 WIB",
-    tanggal_seo: "2026-07-12", // <-- WAJIB TAMBAHKAN BARIS INI UNTUK NAMA FILE BARU
+    update: "Minggu 13 Juli 2026 12:23 WIB",
+    tanggal_seo: "2026-07-13", // <-- WAJIB TAMBAHKAN BARIS INI UNTUK NAMA FILE BARU
     data: {
         prediksi: `
 2065 : 37
 1207 : 64
 6126 : 85
 6781 : 02
+0925 : 68
+2665 : 02
 `,
         //Rumus sama ekor mmundu
         //0902 : 
@@ -22,6 +24,7 @@ const DATA_CAMBODIA = {
 //6126 : 0285 085 85
 //6781 : 5602 502 02
 //0925 : 6028 608 68
+//2665 : 6028 028 02
        
     result2d: "60*06*69*96*80*08*89*98",
     bbfs: "690872",

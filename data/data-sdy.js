f17e90709238015f17437eee999291d8c915ed22
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-13 16:59:46 +0700

    Fix formatting in data-sdy.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-sdy.js
+++ data/data-sdy.js
@@ -1,8 +1,8 @@
 const DATA_SDY = {
-    update: "Senin 13 Juli 2026 14:23 WIB",
-    tanggal_seo: "2026-07-14",
+   update: "Senin 13 Juli 2026 14:23 WIB",
+   tanggal_seo: "2026-07-14",
     data: {
-    prediksi: `
+prediksi: `
 7683 : 61
 5206 : 65
 8652 : 60

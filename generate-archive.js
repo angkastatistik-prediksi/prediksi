5e6a7a76f72bc32ceb35eb123443c5dcf763072f
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-13 20:29:31 +0700

    Update generate-archive.js
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- generate-archive.js
+++ generate-archive.js
@@ -1,5 +1,17 @@
 const fs = require("fs");
 const path = require("path");
+const DOMAIN = "https://angkastatistik.my.id";
+
+const PASARAN = [
+"hk",
+"sdy",
+"sgp",
+"china",
+"japan",
+"taiwan",
+"cambodia",
+"macau"
+];
 
 function generateSitemap(){}
 function generateArchive(){}
@@ -9,3 +21,12 @@ generateArchive();
 generateSitemap();
 generateRSS();
 updateHistory();
+
+const DATA_FOLDER = "./data";
+const OUTPUT_FOLDER = "./arsip";
+if (!fs.existsSync(OUTPUT_FOLDER)){
+    fs.mkdirSync(OUTPUT_FOLDER);
+}
+const today = new Date().toISOString().slice(0,10);
+console.log("Tanggal :",today);
+console.log("Mulai Generate...");

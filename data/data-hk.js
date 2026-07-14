ec404ce9bde53efb710478d7898da5701cd9f52f
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-14 08:22:21 +0700

    Update DATA_HK with new date and results
    Signed-off-by: ANGKA STATISTIK <angkastatistik@gmail.com>

--- data/data-hk.js
+++ data/data-hk.js
@@ -1,6 +1,6 @@
 const DATA_HK = {
-tanggal: "2026-07-14",
-update: "Selasa 14 Juli 2026 08:16 WIB",
+tanggal: "2026-07-11",
+update: "Sabtu 11 Juli 2026 23:35 WIB",
 data: {
 prediksi: `
 8178 : 15
@@ -8,8 +8,7 @@ prediksi: `
 5054 : 42
 4152 : 21
 0161 : 93
-5490 : -87
-9392 : 69
+5490 : 87
 `,
 //2066 : 7649 jgn sama
 //8172 : 4956 956 jgn sama
@@ -26,10 +25,9 @@ prediksi: `
 //4152 : 2931 231 21 tunggal ai 2,3 di ai 3 digit yg dluan itu lemah
 //0161 : 9315 931 93 contoh 93 top 9 vs 4d yg tdk ada angka 3, 2 dan 1 krn 2 dan 1 ada bersama angka 3 di ai 3digit krn 3 sdh lemah
 //5490 : 6987 987 87
-//9392 : 5169 569 69 top
 //Sama ekor atas mundur jgn sama ekor atas
-result2d: "64*46*61*16*65*56",
-bbfs: "64159",
-cb: "6"
+result2d: "84*48*85*58*74*47*75*57",
+bbfs: "845701",
+cb: "7"
 }
 };
